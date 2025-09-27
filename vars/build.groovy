4631950b6d834d4e222f62875fc60e71103694a5
Saurav-patel <khiladisaurav59@gmail.com>
2025-09-27 11:42:10 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,5 @@
-def call(String image_name, String folder){
-  sh "docker build -t ${image_name} ${folder}"
+def call(String image_name,String tag_name ,String folder){
+  sh "docker build -t ${image_name}:${tag_name} ${folder}"
 }
 
 
